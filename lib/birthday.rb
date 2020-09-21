c4586dc3c1144136d01ac05bb70d8729a3eedb2a
Taylor Booth <taylorkbooth@gmail.com>
2020-09-21 01:00:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,12 @@
 #   "Amanda" => 27
 # }
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
