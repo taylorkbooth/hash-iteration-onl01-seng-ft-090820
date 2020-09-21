3b3eb4093cb61f15863681e45e853c89a065640f
Taylor Booth <taylorkbooth@gmail.com>
2020-09-21 01:00:25 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,19 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids name, age|
+  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    elsif 
+      puts "You are too old for this."
+    end
+  end
+  birthday_kids
 end
 
